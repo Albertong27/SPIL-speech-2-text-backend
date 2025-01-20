bc898c967af6cb569b007c89e1e2d297f584690a
Michael Wayne <michaelwaynewibisono@gmail.com>
2025-01-20 15:51:18 +0700

    fix: remove mic-transcribe

---
+++ README.md
@@ -0,0 +1,17 @@
+```
+echo "
+{
+  "type": "",
+  "project_id": "",
+  "private_key_id": "",
+  "private_key": "",
+  "client_email": "",
+  "client_id": "",
+  "auth_uri": "",
+  "token_uri": "",
+  "auth_provider_x509_cert_url": "",
+  "client_x509_cert_url": "",
+  "universe_domain": ""
+}
+" > aiproject-itdev-0190cfc63da2.json
+```
--- server.py
+++ server.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, WebSocket
 from fastapi.websockets import WebSocketDisconnect
 import asyncio
 import uvicorn
-import sounddevice
 from amazon_transcribe.client import TranscribeStreamingClient
 from amazon_transcribe.handlers import TranscriptResultStreamHandler
 from amazon_transcribe.model import TranscriptEvent
@@ -23,35 +22,13 @@ class MyEventHandler(TranscriptResultStreamHandler):
                 for item in alt.items:
                     if item.item_type == "pronunciation":
                         raw_word = item.content
-                        print(f"Raw: {raw_word}")
+                        print(f"raw: {raw_word}")
                         if self.websocket:
                             await self.websocket.send_text(raw_word)
                 transcript = alt.transcript.strip()
-                print(f"Speaker: {transcript}")
+                print(f"final: {transcript}")
                 if self.websocket:
                     await self.websocket.send_text(f"final: {transcript}")
-
-
-async def mic_audio_stream():
-    loop = asyncio.get_event_loop()
-    input_queue = asyncio.Queue()
-
-    def callback(indata, frame_count, time_info, status):
-        loop.call_soon_threadsafe(input_queue.put_nowait, (bytes(indata), status))
-
-    stream = sounddevice.RawInputStream(
-        channels=1,
-        samplerate=16000,
-        callback=callback,
-        blocksize=512,
-        dtype="int16",
-    )
-
-    with stream:
-        while True:
-            indata, status = await input_queue.get()
-            yield indata, status
-
 
 async def browser_audio_stream(websocket: WebSocket, transcribe_stream):
     try:
@@ -62,24 +39,6 @@ async def browser_audio_stream(websocket: WebSocket, transcribe_stream):
         print("WebSocket disconnected")
         await transcribe_stream.input_stream.end_stream()
 
-
-async def write_chunks(transcribe_stream):
-    async for chunk, status in mic_audio_stream():
-        await transcribe_stream.input_stream.send_audio_event(audio_chunk=chunk)
-    await transcribe_stream.input_stream.end_stream()
-
-
-async def mic_transcribe(websocket):
-    client = TranscribeStreamingClient(region="us-east-1")
-    stream = await client.start_stream_transcription(
-        language_code="id-ID",
-        media_sample_rate_hz=16000,
-        media_encoding="pcm",
-    )
-    handler = MyEventHandler(stream.output_stream, websocket)
-    await asyncio.gather(write_chunks(stream), handler.handle_events())
-    
-
 async def browser_transcribe(websocket):
     client = TranscribeStreamingClient(region="us-east-1")
     stream = await client.start_stream_transcription(
@@ -87,26 +46,14 @@ async def browser_transcribe(websocket):
         media_sample_rate_hz=16000,
         media_encoding="pcm",
     )
-    handler = MyEventHandler(stream.output_stream, websocket)   
+    handler = MyEventHandler(stream.output_stream, websocket)
     await asyncio.gather(browser_audio_stream(websocket, stream), handler.handle_events())
-
 
 @app.get("/")
 def root():
-    return {"Hola" : "SPIL"}
+    return {"Hola": "SPIL"}
 
-
-@app.websocket("/mic-transcribe")
-async def websocket_handler_mic(websocket: WebSocket):
-    await websocket.accept()
-    try:
-        print("Client connected")
-        await mic_transcribe(websocket)
-    except WebSocketDisconnect:
-        print("Client disconnected")
-
-
-@app.websocket("/browser-transcribe")
+@app.websocket("/aws-browser-transcribe")
 async def websocket_handler_browser(websocket: WebSocket):
     await websocket.accept()
     try:
@@ -115,7 +62,5 @@ async def websocket_handler_browser(websocket: WebSocket):
     except WebSocketDisconnect:
         print("Client disconnected")
 
-
-
 if __name__ == "__main__":
-    uvicorn.run("server_api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
