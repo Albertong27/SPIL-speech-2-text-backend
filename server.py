955d929c1eec2992e29f21b96cbc2b1e174ac908
Michael Wayne <michaelwaynewibisono@gmail.com>
2025-01-22 20:23:50 +0700

    feat: convert to txt file

--- server.py
+++ server.py
@@ -5,15 +5,17 @@ import uvicorn
 from amazon_transcribe.client import TranscribeStreamingClient
 from amazon_transcribe.handlers import TranscriptResultStreamHandler
 from amazon_transcribe.model import TranscriptEvent
+from datetime import datetime
 
 app = FastAPI()
 
 class MyEventHandler(TranscriptResultStreamHandler):
-    def __init__(self, stream, websocket: WebSocket):
+    def __init__(self, stream, websocket: WebSocket, filepath: str):
         super().__init__(stream)
         self.websocket = websocket
         self.partial_sentence = []
         self.last_partial_transcript = ""
+        self.filepath = filepath
 
     async def handle_transcript_event(self, transcript_event: TranscriptEvent):
         for sentence in transcript_event.transcript.results:
@@ -34,6 +36,8 @@ class MyEventHandler(TranscriptResultStreamHandler):
                 print(f"Final: {final_sentence}")
                 if self.websocket:
                     await self.websocket.send_text(f"final: {final_sentence}")
+                    with open(self.filepath, mode='a', encoding='UTF-8') as output:
+                        print(final_sentence, file=output)
                 self.partial_sentence = []
                 self.last_partial_transcript = ""
 
@@ -46,14 +50,14 @@ async def browser_audio_stream(websocket: WebSocket, transcribe_stream):
         print("WebSocket disconnected")
         await transcribe_stream.input_stream.end_stream()
 
-async def browser_transcribe(websocket):
+async def browser_transcribe(websocket, filepath):
     client = TranscribeStreamingClient(region="us-east-1")
     stream = await client.start_stream_transcription(
         language_code="id-ID",
         media_sample_rate_hz=16000,
         media_encoding="pcm",
     )
-    handler = MyEventHandler(stream.output_stream, websocket)
+    handler = MyEventHandler(stream.output_stream, websocket, filepath)
     await asyncio.gather(browser_audio_stream(websocket, stream), handler.handle_events())
 
 @app.get("/")
@@ -65,7 +69,8 @@ async def websocket_handler_browser(websocket: WebSocket):
     await websocket.accept()
     try:
         print("Client connected")
-        await browser_transcribe(websocket)
+        filepath = datetime.now().strftime("%d-%m-%Y %H.%M.%S")
+        await browser_transcribe(websocket, filepath)
     except WebSocketDisconnect:
         print("Client disconnected")
 
